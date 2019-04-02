ec924fbd48ff3a5d36b9e8f6ff44a34591d3dd17
Igor <isrpo@yandex.ru>
2019-04-02 20:06:02 +0300

    handler for errors

--- js/handler.js
+++ js/handler.js
@@ -1,21 +1,38 @@
-let access_token = location.href.substring((location.href.indexOf('=') + 1), location.href.indexOf('&ex'));
-let user_id = location.href.substring((location.href.indexOf('id=') + 3));
-// let xhr = new XMLHttpRequest();
-// xhr.open('GET', 'https://api.vk.com/method/friends.search?user_id=' +
-//     user_id + '&fields=photo_50&count=5&access_token=' + access_token, true);
-// xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-// xhr.send();
-// xhr.onload = function () {
-//     if (xhr.status != 200) {
-//         alert('Error! Status: ' + xhr.status);
-//     } else {
-//         console.log(xhr.responseText);
-//     }
-// }
-let script = document.createElement('SCRIPT');
-script.src = 'https://api.vk.com/method/friends.search?user_id=' + user_id + '&fields=photo_50&count=5&access_token=' +
-    access_token + '&v=5.92&callback=callbackFunc';
-document.getElementsByTagName("head")[0].appendChild(script);
+function handler() {
+    alert(location.href.indexOf('access_token='));
+    if (location.href.indexOf('access_token=') === -1 || location.href.indexOf('id=') === -1) {
+        returnToMainPage();
+    } else {
+        let access_token = location.href.substring((location.href.indexOf('access_token=') + 13),
+            location.href.indexOf('&ex'));
+        let user_id = location.href.substring((location.href.indexOf('id=') + 3));
+        alert(access_token);
+        alert(user_id);
+        let script = document.createElement('SCRIPT');
+        script.src = 'https://api.vk.com/method/friends.search?user_id=' + user_id + '&fields=photo_50&count=5&access_token=' +
+            access_token + '&v=5.92&callback=callbackFunc';
+        document.getElementsByTagName("head")[0].appendChild(script);
+    }
+}
+
 function callbackFunc(result) {
-    console.log(result.response);
+    if (result.response !== undefined || result.response != null) {
+        console.log(result.response);
+    } else {
+        returnToMainPage();
+    }
+}
+
+function returnToMainPage() {
+    let textErr = document.createElement('div');
+    let pTextErr = document.createElement('p');
+    pTextErr.innerHTML = 'Что-то пошло не так...';
+
+    let returnLink = document.createElement('a');
+    returnLink.href = 'index.html';
+    returnLink.innerHTML = 'Вернуться на главную';
+
+    document.body.appendChild(textErr);
+    textErr.appendChild(pTextErr);
+    textErr.appendChild(returnLink);
 }
