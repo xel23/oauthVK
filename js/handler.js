b5efcd478cf6b5a7ebaf6aeac435dd0a7d4b323f
Igor <isrpo@yandex.ru>
2019-04-02 12:52:49 +0300

    make callback JSONP

--- js/handler.js
+++ js/handler.js
@@ -1,14 +1,21 @@
 let access_token = location.href.substring((location.href.indexOf('=') + 1), location.href.indexOf('&ex'));
-let user_id = location.href.substr((location.href.indexOf('user_id=') + 1));
-let xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://api.vk.com/method/friends.search?user_id=' +
-    user_id + '&fields=photo_50&count=5&access_token=' + access_token + '&v=5.92', true);
-xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-xhr.send();
-xhr.onload = function () {
-    if (xhr.status != 200) {
-        alert('Error! Status: ' + xhr.status);
-    } else {
-        console.log(xhr.responseText);
-    }
+let user_id = location.href.substring((location.href.indexOf('id=') + 3));
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://api.vk.com/method/friends.search?user_id=' +
+//     user_id + '&fields=photo_50&count=5&access_token=' + access_token, true);
+// xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+// xhr.send();
+// xhr.onload = function () {
+//     if (xhr.status != 200) {
+//         alert('Error! Status: ' + xhr.status);
+//     } else {
+//         console.log(xhr.responseText);
+//     }
+// }
+let script = document.createElement('SCRIPT');
+script.src = 'https://api.vk.com/method/friends.search?user_id=' + user_id + '&fields=photo_50&count=5&access_token=' +
+    access_token + '&v=5.92&callback=callbackFunc';
+document.getElementsByTagName("head")[0].appendChild(script);
+function callbackFunc(result) {
+    console.log(result.response);
 }
